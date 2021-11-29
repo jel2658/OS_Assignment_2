70c3723e2effec060b93951833fbec1af4892e84
Justin L <jel2658@gmail.com>
2021-11-28 22:55:56 -0500

    Finished main.cpp

--- OS_Assignment_2/main.cpp
+++ OS_Assignment_2/main.cpp
@@ -34,35 +34,123 @@ int main() {
 			num2 = num2.substr(num + 1);
 			instances[i] = stoi(num2);
 		}*/
-		
+
 		int Available[resourceTypes];
 		int Max[processes][resourceTypes];
 		int Allocation[processes][resourceTypes];
 		int Need[processes][resourceTypes];
 
-		size_t temp;
-		string temp2;
+		size_t temp = 0;
+		string temp2 = line[2]; // "0 1 0"
 
-		int toNext = 2 + processes;
+		int toNext = processes;
 
-		for (int i = 2; i < toNext; i++) {
+		for (int i = 0; i < toNext; i++) {
+			temp2 = line[2 + i];
 			for (int j = 0; j < resourceTypes; j++) {
 
-				temp = line[i].find_first_of("0123456789");
-				temp2 = line[i].substr(temp);
 				Allocation[i][j] = stoi(temp2);
-				cout << Allocation[i][j];
-				temp++;
+				temp = temp2.find_first_of("0123456789");
+				temp2 = temp2.substr(++temp);
+				//cout << temp2;
+				//cout << Allocation[i][j] << " ";
 			}
-			cout << "\n";
+			//cout << "\n";
 		}
 
 		toNext = toNext + processes;
 
-		{
+		temp = 0;
+		temp2 = line[3 + processes];
+		//cout << temp2 << "\n";
+
+		for (int i = 0; i < processes; i++) {
+			temp2 = line[3 + processes + i];
+			for (int j = 0; j < resourceTypes; j++) {
+
+				Max[i][j] = stoi(temp2);
+				temp = temp2.find_first_of("0123456789");
+				temp2 = temp2.substr(++temp);
+				//cout << Max[i][j] << " ";
+			}
+			//cout << "\n";
+		}
+
+		temp = 0;
+		temp2 = line[9 + processes];
+		//cout << temp2 << "\n";
+
+		for (int i = 0; i < resourceTypes; i++) {
+
+			Available[i] = stoi(temp2);
+			temp = temp2.find_first_of("0123456789");
+			temp2 = temp2.substr(++temp);
+			//cout << Available[i] << " ";
+		}
+
+		for (int i = 0; i < processes; i++) {
+			for (int j = 0; j < resourceTypes; j++) {
+				Need[i][j] = Max[i][j] - Allocation[i][j];
+			}
+		}
+
+		bool safe = false;
 			int Work[resourceTypes];
 			bool Finish[processes];
-		}
+			int sequence[processes];
+			int safeProcesses = 0;
+
+			for (int i = 0; i < processes; i++) {
+				Finish[i] = false;
+			}
+
+			for (int i = 0; i < resourceTypes; i++) {
+				Work[i] = Available[i];
+			}
+
+			while (safeProcesses < processes) {
+				for (int i = 0; i < processes; i++) {
+					if (!Finish[i]) {
+						int j = 0;
+						while (j < resourceTypes) {
+							if (!(Need[i][j] <= Work[j])) {
+								break;
+							}
+							j++;
+						}
+						if (j == resourceTypes) {
+							for (int index = 0; index < resourceTypes; index++) {
+								Work[index] = Work[index] + Allocation[i][index];
+							}
+							sequence[safeProcesses] = i;
+							safeProcesses++;
+							Finish[i] = true;
+							safe = true;
+						}
+					}
+				}
+				
+			}
+			//cout << safe << "\n";
+			for (int i = 0; i < processes; i++) {
+				//cout << Finish[i] << "\n";
+				if (!Finish[i]) {
+					safe = false;
+					break;
+				}
+			}
+
+			if (safe) {
+				cout << "System is in a safe state.\n";
+				cout << "Safe sequence of processes is: \n";
+				for (int i = 0; i < processes; i++) {
+					cout << sequence[i] << " ";
+				}
+			}
+			else {
+				cout << "System is not in a safe state.\n";
+			}
+		
 	}
 	else {
 		cout << "Unable to open input file.";
