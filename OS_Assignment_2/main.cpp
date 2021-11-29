cf99111ddafb43fa1c748d00a9e0af43c6c84d07
Justin L <jel2658@gmail.com>
2021-11-28 20:50:18 -0500

    Work on main and input

--- OS_Assignment_2/main.cpp
+++ OS_Assignment_2/main.cpp
@@ -8,9 +8,9 @@ int main() {
 
 	if (file.is_open()) {
 
-		string line[2];
+		string line[3];
 		for (int i = 0; getline(file, line[i]); i++) {
-			cout << "Getting lines...";
+			cout << "Getting lines...\n";
 		}
 
 		size_t num = line[0].find_last_not_of("0123456789");
@@ -18,7 +18,22 @@ int main() {
 		//cout << num2;
 		processes = stoi(num2);
 		//cout << processes;
+		num = line[1].find_last_not_of("0123456789");
+		num2 = line[1].substr(num + 1);
+		resourceTypes = stoi(num2);
+		//cout << resourceTypes;
+		num = line[2].find_first_of("0123456789");
+		num2 = line[2].substr(num);
+		//cout << num2;
 
+		int instances[10];
+		instances[0] = stoi(num2);
+		for (int i = 1; i < resourceTypes; i++) {
+			num = num2.find_first_of(" ");
+			num2 = num2.substr(num + 1);
+			instances[i] = stoi(num2);
+		}
+		
 
 	}
 	else {
